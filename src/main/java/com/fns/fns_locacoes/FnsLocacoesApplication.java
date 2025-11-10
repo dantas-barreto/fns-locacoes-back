9e99d934033d7d198f243dd88129a6fe139f6c8c
Joao Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2025-11-10 15:45:35 -0300

    Começo de projeto backend

---
+++ src/main/java/com/fns/fns_locacoes/FnsLocacoesApplication.java
@@ -0,0 +1,13 @@
+package com.fns.fns_locacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnsLocacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnsLocacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fns/fns_locacoes/FnsLocacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fns.fns_locacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnsLocacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
